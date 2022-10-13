3d955e801ff3aa95352bfe3dea72c77a85e2c92a
lztech-tp <62520358+lztech-tp@users.noreply.github.com>
2022-10-12 20:28:47 -0400

    update requirement file

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ import numpy as np
 from numpy import loadtxt
 import pickle
 import matplotlib.pyplot as plt
-import streamlit as st
 import xgboost as xgb
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.metrics import confusion_matrix
